e35570d87fd9db08a549ea6d32d70d5f5e57387d
1000channel <ponnge@gmail.com>
2020-11-08 23:12:11 +0900

    バリデーション、テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date, presence: true
-       
+
+  with_options presence: true do
+    validates :nickname
+    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX }
+    validates :birth_date
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end
