8dd482d4d6b4c803033f2f5a2c460f328db19895
1000channel <ponnge@gmail.com>
2020-11-12 19:57:47 +0900

    Javascript計算結果表示を実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const addTaxDom = document.getElementById("add-tax-price");
+        const addTaxValue=(Math.floor(inputValue * 0.1));
+        addTaxDom.innerHTML = addTaxValue;
+
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(inputValue - addTaxValue)
+  })
+   });
