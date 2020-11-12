a0f01801425c013fb5bfebcb0b124e55a3f6258e
1000channel <ponnge@gmail.com>
2020-11-12 18:12:49 +0900

    itemモデル単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Lorem.sentence}
+    info {Faker::Lorem.sentence}
+    category_id { 1 }
+    sales_status_id { 1 }
+    shipping_fee_status_id { 1 }
+    prefecture_id { 1 }
+    scheduled_delivery_id { 1 }
+    price { 5555 }
+
+    association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
