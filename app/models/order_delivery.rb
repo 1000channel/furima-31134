2744341d2541218b746c49245c7202f9c69cdb5b
1000channel <ponnge@gmail.com>
2020-11-17 18:54:09 +0900

    Formオブジェクトを作成

---
+++ app/models/order_delivery.rb
@@ -0,0 +1,23 @@
+class OrderDelivery
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :addresses
+
+    with_options numericality: { other_than: 0 } do
+      validates :prefecture_id
+    end
+
+    validates :phone, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order_id)
+  end
+
+  
+end
