62d2d89a63e7c7587fdfe126e8c380f906d9ebfb
1000channel <ponnge@gmail.com>
2020-11-18 18:56:45 +0900

    テストコードを実装

---
+++ spec/factories/order_deliveries.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_delivery do
+    postal_code { '914-0141' }
+    prefecture_id { 18 }
+    city { '敦賀市' }
+    addresses { '莇生野58-24-2' }
+    building { '503' }
+    phone_number { '08029563297' }
+  end
+end
